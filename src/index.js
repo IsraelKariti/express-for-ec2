0557537664b2c931fc369cee88b4d53e02316d15
israelkariti <israelkariti@gmail.com>
2024-12-04 13:23:54 +0200

    post and get

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/', async (req, res)=>{
+app.post('/', async (req, res)=>{    
+    
     const message = await Computer.create(req.body);
     res.status(200).send('success');
 });
 
 app.get('/', async (req, res)=>{
-
+    const computers = await Computer.findAll();
+    res.status(200).send(computers);
 });
 
 (async ()=>{
